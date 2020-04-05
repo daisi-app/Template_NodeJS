72f7e4de8f4428a33d6afe3d3da9e3f236c87085
Daisi <57485790+daisi-app@users.noreply.github.com>
2020-04-05 21:39:16 +0200

    feat(Project): mise à jour du template + Ajout de helmet

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const app = require("express")();
-const bodyParser = require("body-parser");
-const config = require("./config");
+'use strict';
+const app = require('express')();
+const bodyParser = require('body-parser');
+const helmet = reuqire('helmet');
+const config = require('./config');
 
-console.log("/ *************** Server Start *************** \\");
+console.log('/ *************** Server Start *************** \\');
+const serverPort = config.server.port || 8080;
+console.log(`Starting server on port ${serverPort} ...`);
 
 // Initialisations
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
-app.get("/health", (req, res) => res.sendStatus(200));
+app.get('/health', (req, res) => res.sendStatus(200));
 
 // Services Implementations
-app.use("/api", require("./src/routes"));
+app.use('/api', require('./src/routes'));
 
 // Error 404
 app.use((req, res) => res.sendStatus(404));
@@ -20,4 +25,7 @@ app.use((req, res) => res.sendStatus(404));
 app.use((err, req, res, next) => res.sendStatus(500));
 
 // Server Start with the port in config file
-app.listen(config.server.port, () => console.log(`Server start on port : ${config.server.port}`));
+app.listen(serverPort, async () => {
+    console.log('Server started');
+    console.log('***************************************************************');
+});
